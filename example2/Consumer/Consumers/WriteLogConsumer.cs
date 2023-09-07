bc84ba542bb1aa85f9086fbb12b4e305b17a91ff
Paweł Worwąg <pawel.worwag@gmail.com>
2023-09-07 21:28:16 +0200

    Consumer configuration.
    WriteLog queue has been created.
    Messages:WriteLog and WriteLog has been created.

--- example2/Consumer/Consumers/WriteLogConsumer.cs
+++ example2/Consumer/Consumers/WriteLogConsumer.cs
@@ -7,14 +7,14 @@ public class WriteLogConsumer : IConsumer<Messages.WriteLog>
 {
     private readonly ILogger _logger;
 
-    public WriteLogConsumer(ILogger logger)
+    public WriteLogConsumer(ILogger<WriteLogConsumer> logger)
     {
         _logger = logger;
     }
 
     public Task Consume(ConsumeContext<WriteLog> context)
     {
-        _logger.LogInformation("Incommeing message");
+        _logger.LogInformation("Incoming message");
         return Task.CompletedTask;
     }
 }
