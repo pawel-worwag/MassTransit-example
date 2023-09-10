5b23aa5ac0066870fb457ac06b1c46a6cb055259
Paweł Worwąg <pawel.worwag@gmail.com>
2023-09-10 11:53:49 +0200

    RabbitMQ transport

--- example3/sample1/Consumer/Program.cs
+++ example3/sample1/Consumer/Program.cs
@@ -1,4 +1,18 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOptions<RabbitMqTransportOptions>()
+    .Configure(options =>
+    {
+        options.Host = builder.Configuration.GetValue<string>("Rabbit:Host");
+        options.Port = builder.Configuration.GetValue<ushort>("Rabbit:Port");
+        options.VHost = builder.Configuration.GetValue<string>("Rabbit:VHost");
+        options.User = builder.Configuration.GetValue<string>("Rabbit:User");
+        options.Pass = builder.Configuration.GetValue<string>("Rabbit:Pass");
+        options.UseSsl = builder.Configuration.GetValue<bool>("Rabbit:UseSsl");
+    });
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
